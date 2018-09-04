8561ffca8ffbcccc078afc5ca04e5f7f73e382fe
Michael Bright <github.com@mjbright.net>
2018-09-04 18:18:07 +0200

    docker_build script builds different demo versions
    also optionally pushes to docker hub

--- docker_build.sh
+++ docker_build.sh
@@ -1,1 +1,30 @@
-docker build -t mjbright/flask-web:v1 .
+#!/bin/bash
+
+HUB_PUSH=0
+
+# Build all three versions of our application:
+
+[ "$1" = "-push" ] && HUB_PUSH=1
+
+[ $HUB_PUSH -ne 0 ] && {
+    CMD="docker login"
+    echo "-- $CMD"
+    $CMD
+}
+
+for version in 0 1 2 3; do
+    cp -a versions/app.py.v$version app.py
+
+    CMD="docker build -t mjbright/flask-web:v$version ."
+    echo "-- $CMD"
+    $CMD
+
+    [ $HUB_PUSH -ne 0 ] && {
+        CMD="docker push mjbright/flask-web:v$version"
+        echo "-- $CMD"
+        $CMD
+    }
+done
+
+docker search mjbright/flask-web
+
