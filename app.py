a183db49bf45a9e64fc9a6168149755eab37b511
Michael Bright <github.com@mjbright.net>
2018-10-02 18:35:08 +0200

    Set host/port from env variables or default values

--- app.py
+++ app.py
@@ -1,32 +1,20 @@
-from flask import Flask, request, Response, stream_with_context, jsonify
-from redis import Redis
+
 import os, time, json
+import socket
+
+from redis import Redis
+from flask import Flask, request, Response, stream_with_context, jsonify
+
 app = Flask(__name__)
-db = Redis(host='redis', port=6379)
+db = Redis( host = os.getenv( 'REDIS', 'redis'),
+            port = int(os.getenv( 'REDIS_PORT', '6379' ) )
 
 @app.route('/')
 def hello():
     db.incr('count')
-    return 'Count is %s.' % db.get('count')
 
-
-@app.route('/<path:path>', methods = ['PUT', 'GET'])
-def home(path):
-
-    if (request.method == 'PUT'):
-        event = request.json
-        event['last_updated'] = int(time.time())
-        db.delete(path) #remove old keys
-        db.hmset(path, event)
-        return jsonify(event), 201
-
-
-    if not db.exists(path):
-        return "Error: path does not exist"
-
-    event = db.hgetall(path)
-    return jsonify(event), 200
-
-
+    host = socket.gethostname()
+    return '''[%s] Redis counter value=%s\n''' % (host, db.get('count'))
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
