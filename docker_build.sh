1ad859fd23845dfb4e9c1ee6f82f03c0d5417da2
Michael Bright <github.com@mjbright.net>
2018-10-02 18:34:22 +0200

    Use variables for image name, versions

--- docker_build.sh
+++ docker_build.sh
@@ -2,6 +2,10 @@
 
 HUB_PUSH=0
 
+OWNER="mjbright"
+APP="flask-web"
+VERSIONS="0 1 2 3"
+
 # Build all three versions of our application:
 
 [ "$1" = "-push" ] && HUB_PUSH=1
@@ -12,19 +16,19 @@ HUB_PUSH=0
     $CMD
 }
 
-for version in 0 1 2 3; do
+for version in $VERSIONS; do
     cp -a versions/app.py.v$version app.py
 
-    CMD="docker build -t mjbright/flask-web:v$version ."
+    CMD="docker build -t $OWNER/$APP:v$version ."
     echo "-- $CMD"
     $CMD
 
     [ $HUB_PUSH -ne 0 ] && {
-        CMD="docker push mjbright/flask-web:v$version"
+        CMD="docker push $OWNER/$APP:v$version"
         echo "-- $CMD"
         $CMD
     }
 done
 
-docker search mjbright/flask-web
+docker search $OWNER/$APP
 
